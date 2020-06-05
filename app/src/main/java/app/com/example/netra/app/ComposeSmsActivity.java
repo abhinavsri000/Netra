4fbf52e5278677f6e6fe023548e62dcb69afdbca
abhinav <abhinavsri000@gmail.com>
2020-06-05 21:58:16 +0530

    init

---
+++ app/src/main/java/app/com/example/netra/app/ComposeSmsActivity.java
@@ -0,0 +1,8 @@
+package app.com.example.netra.app;
+
+import android.app.Activity;
+
+public class ComposeSmsActivity extends Activity
+	{
+    // TODO: Implement SMS/MMS compose functions and need to respond ACTION_SENDTO Intnet
+	}
---
+++ app/src/main/java/app/com/example/netra/app/EditTextImeBackListener.java
@@ -0,0 +1,6 @@
+package app.com.example.netra.app;
+
+public interface EditTextImeBackListener
+	{
+	public abstract void onImeBack(EditTextBackEvent ctrl, String text);
+	}
